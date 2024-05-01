726e6496821be9299fb49ce21ec87104fcabc52e
shwetangi03 <113777115+shwetangi03@users.noreply.github.com>
2024-05-02 00:21:24 +0530

    redirecting user after the password is changed

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,11 +1,11 @@
-import { useState, useRef,useContext } from "react";
-
+import { useState, useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
 import classes from "./AuthForm.module.css";
 import AuthContext from "../../store/auth-context";
 
 const AuthForm = () => {
-
-  const authCtx = useContext(AuthContext)
+  const authCtx = useContext(AuthContext);
+  const history = useHistory();
 
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
@@ -58,7 +58,8 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        authCtx.login(data.idToken)
+        authCtx.login(data.idToken);
+        history.replace("/");
       })
       .catch((err) => {
         alert(err.message);
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,12 @@
 import React, { useRef, useContext } from "react";
-
+import { useHistory } from "react-router-dom";
 import classes from "./ProfileForm.module.css";
 import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
   const authCtx = useContext(AuthContext);
   const newPasswordInputRef = useRef();
+  const history = useHistory();
 
   const SubmitHandler = (event) => {
     event.preventDefault();
@@ -24,13 +25,20 @@ const ProfileForm = () => {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then((res) => {
+      history.replace("/");
+    });
   };
   return (
     <form className={classes.form} onSubmit={SubmitHandler}>
       <div className={classes.control}>
         <label htmlFor="new-password">New Password</label>
-        <input type="password" id="new-password" minLength="7" ref={newPasswordInputRef} />
+        <input
+          type="password"
+          id="new-password"
+          minLength="7"
+          ref={newPasswordInputRef}
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
