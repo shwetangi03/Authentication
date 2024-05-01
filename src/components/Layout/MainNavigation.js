56bb8dfb4afbf7f50b13a6dfca191b1c15cedf1a
shwetangi03 <113777115+shwetangi03@users.noreply.github.com>
2024-05-02 00:32:48 +0530

    Implementing Logout functionality

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,30 +1,40 @@
-import { Link } from 'react-router-dom';
-import React,{useContext} from 'react';
-import classes from './MainNavigation.module.css';
-import AuthContext from '../../store/auth-context';
+import { Link,useHistory } from "react-router-dom";
+import React, { useContext } from "react";
+import classes from "./MainNavigation.module.css";
+import AuthContext from "../../store/auth-context";
 
 const MainNavigation = () => {
+  const authCtx = useContext(AuthContext);
+  const history= useHistory()
+  const isLoggedIn = authCtx.isLoggedIn;
 
-  const authCtx=useContext(AuthContext)
-
-  const isLoggedIn= authCtx.isLoggedIn
+  const logoutHandler = ()=>{
+    authCtx.logout()
+    history.replace("/")
+  }
 
   return (
     <header className={classes.header}>
-      <Link to='/'>
+      <Link to="/">
         <div className={classes.logo}>React Auth</div>
       </Link>
       <nav>
         <ul>
-          {!isLoggedIn &&<li>
-            <Link to='/auth'>Login</Link>
-          </li>}
-          {isLoggedIn&&<li>
-            <Link to='/profile'>Profile</Link>
-          </li>}
-          {isLoggedIn &&<li>
-            <button>Logout</button>
-          </li>}
+          {!isLoggedIn && (
+            <li>
+              <Link to="/auth">Login</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
